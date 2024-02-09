350aed4ac49ed189a7f282da2fa5d87a7618fed5
abijith-tk <abijithtk1212@gmail.com>
2024-02-10 00:00:07 +0530

    styles

--- play.js
+++ play.js
@@ -7,16 +7,19 @@ export default function playing(){
             <View style={styles.nowPlaying}>
                 <Text style={styles.head}>Now Playing</Text>
                 <View style={styles.card}>
-                    <Text>Song1</Text>
+                    <Text>Song 1</Text>
+                    <Text>Artist 1</Text>
                 </View>
             </View>
 
             <View style={styles.upcoming}>
                 <View style={styles.card}>
                     <Text>Song 2</Text>
+                    <Text>Artist 2</Text>
                 </View>
                 <View style={styles.card}>
                     <Text>Song 3</Text>
+                    <Text>Artist 3</Text>
                 </View>
             </View>
         </View>
@@ -29,5 +32,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+      },
+      card : {
+        backgroundColor:'white',
+        width:'60%',
       },
 })
