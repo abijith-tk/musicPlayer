3c51007112351e4dc5f9cca6394d90711d7fd387
abijith-tk <abijithtk1212@gmail.com>
2024-02-12 19:10:53 +0530

    Background image property added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, ImageBackground } from 'react-native';
-import { result } from './result';
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground, Image } from 'react-native';
+import { result } from './components/result';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ImageBackground source={require('./assets/background.png')} style={styles.container}>
+        <Image source={require('./assets/desi-cuppa.png')}/>
         <Text style={styles.heading}>Play Your Tune!</Text>
         <View style={styles.main}> 
           <TextInput 
@@ -12,41 +13,45 @@ export default function App() {
           style={styles.searchBox}
           />
           <View style={styles.searchBtn}>
-            <Button title='Search' onPress={result}/>
+            <Button title='Search' onPress={result} color='#0d6efd' />
           </View>
         </View>
         <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   heading:{
-    color:'black',
+    color:'white',
     fontWeight:'bold',
-    fontSize:24,
-    fontStyle:'italic'
+    fontSize:28,
+    marginBottom:20
   },
   main : {
     width:'80%',
     justifyContent:'center'
   },
   searchBox:{
-    padding:10,
+    paddingRight:10,
+    paddingLeft:10,
+    paddingTop:5,
+    paddingBottom:5,
     margin:10,
     borderColor:'gray',
     borderWidth:2,
     width:'90%',
-    borderRadius:2,
+    borderRadius:10,
+    backgroundColor:'white'
   },
   searchBtn : {
     margin:10,
-    width:'90%',  
+    width:'90%', 
+    borderRadius:10,
   }
 });
